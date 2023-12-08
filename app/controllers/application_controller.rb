a0f5fa418d25a50d92623597116b33245f7bdd4e
nehasahgal <146212872+nehasahgal@users.noreply.github.com>
2023-12-08 18:11:20 +0000

    profile updating capabilities created - reset commit #3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,13 @@ class ApplicationController < ActionController::Base
   skip_forgery_protection
 
   before_action :authenticate_user!
-  
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, :keys => [:username, :name])
+
+    devise_parameter_sanitizer.permit(:account_update, :keys => [:email])
+  end
+
 end
